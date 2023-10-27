5ed2735b5f2ac833cdd267b027a90e4a68424bed
m1njunK <137128412+m1njunK@users.noreply.github.com>
2023-10-28 08:46:26 +0900

    [level unrated] Title: 자릿수 더하기, Time: 5.10 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String arr[] = (n+"").split("");
+        for(String s : arr){
+            answer += Integer.parseInt(s);
+        }
+        
+        return answer;
+    }
+}
