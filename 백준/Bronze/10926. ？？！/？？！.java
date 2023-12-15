85016600b78622267e73ff8ca17648105332dbf2
m1njunK <137128412+m1njunK@users.noreply.github.com>
2023-12-16 07:00:59 +0900

    [Bronze V] Title: ??!, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,10 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		sb.append(br.readLine());
+		sb.append("??!");
 		
-		String x = sc.next();
-		System.out.println(x+"??!");
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(sb.toString());
+		bw.flush();
 	}
 }
