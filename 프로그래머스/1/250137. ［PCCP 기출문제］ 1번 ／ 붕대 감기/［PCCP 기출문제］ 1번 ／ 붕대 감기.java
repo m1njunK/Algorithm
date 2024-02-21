d7bceb691323594ea217e642cc85171277d48666
m1njunK <137128412+m1njunK@users.noreply.github.com>
2024-02-22 00:03:30 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.08 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -11,21 +11,21 @@ class Solution {
         for(int i = 1; i <= time; i++) {
         	int at = attacks[idx][0]; //공격시간
         	int dmg = attacks[idx][1]; //피해량
-            if(i == at){ //현재시간 == 공격시간
+            if(i == at){ //공격받은시간
                 hp -= dmg; //체력 감소
                 cnt = 0; //연속 성공 횟수 초기화
                 idx++;
-            } else {
-                cnt++;
-                if(cnt == t){
-                    hp += y+x;
-                    cnt = 0;
+            } else { //공격에 성공한 경우
+                cnt++; //연속 성공 횟수 증가
+                if(cnt == t){ //연속 성공 횟수가 시전시간과 같을 경우
+                    hp += y+x; //추가 회복량 만큼 더 회복
+                    cnt = 0; //연속 성공 횟수 초기화
                 } else {
-                    hp+=x;   
+                    hp+=x; //현재체력+=초당 회복량   
                 }
             }
-        	hp = hp > health ? health : hp;
-            if(hp <= 0) {
+        	hp = hp > health ? health : hp; //현재 체력이 최대체력보다 커질경우 최대체력으로 설정
+            if(hp <= 0) { //사망한경우
         		return -1;
         	}
         }
